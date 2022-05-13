a39a8e4b29021d173160e1edf685f86c9b94ef72
DarshKodwani <ddkcdk@yahoo.co.uk>
2022-05-13 18:17:29 +0530

    improve grammar

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,12 +4,12 @@ import numpy as np
 
 def format_ocean_proximity(raw_df):
     """
-    Changes the feature `ocean_proximity` to be lowercase and change the value `<1H OCEAN` to `within_hour_ocean`
+    Changes the feature `ocean_proximity` to be lowercase and change the value `<1H OCEAN` to `within_hour_of_ocean`
     Args:
         raw_df: pandas data frame object with the raw data from the csv
     Returns:
-        formatted_df: pandas df object with the `ocean_proximity` feature with lower case values and `<1H OCEAN` values changed to `within_hour_ocean`
+        formatted_df: pandas df object with the `ocean_proximity` feature with lower case values and `<1H OCEAN` values changed to `within_hour_of_ocean`
     """
-    raw_df["ocean_proximity"] = ["within_hour_ocean" if val == "<1H OCEAN" else val for val in raw_df["ocean_proximity"]]
+    raw_df["ocean_proximity"] = ["within_hour_of_ocean" if val == "<1H OCEAN" else val for val in raw_df["ocean_proximity"]]
     raw_df["ocean_proximity"] = raw_df["ocean_proximity"].apply(lambda x: x.lower().replace(" ", "_"))
     return raw_df
